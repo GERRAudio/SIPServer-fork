3fc8df2b34837f25a8b7310f78810129653b36ae
Tony <158102715+tonyGERR@users.noreply.github.com>
2025-10-08 18:30:24 -0400

    svcstop are you sure

--- SIPServer-Installer/InstallUtils/SvcStop.bat
+++ SIPServer-Installer/InstallUtils/SvcStop.bat
@@ -1,3 +1,17 @@
 @echo off
+CHOICE /C YN /M "Are you sure you want to stop the service and potentially disconnect all calls? "
+IF %ERRORLEVEL% EQU 1 GOTO :Proceed
+IF %ERRORLEVEL% EQU 2 GOTO :ExitScript
+
+
+
+:Proceed
+echo Shutting down...
 powershell -Command "Start-Process cmd -ArgumentList '/c sc Stop SIPServer' -Verb RunAs"
+timeout /t 2 /nobreak > NUL
+goto :EOF
+:ExitScript
+echo You chose to exit without shutdown.
+timeout /t 2 /nobreak > NUL
+:EOF
 exit
