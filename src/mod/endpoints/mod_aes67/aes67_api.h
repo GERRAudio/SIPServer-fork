99e2e98eac6308def1aad9db47b466877e2618c3
Tony <158102715+tonyGERR@users.noreply.github.com>
2025-12-16 12:24:50 -0500

    version date set

--- src/mod/endpoints/mod_aes67/aes67_api.h
+++ src/mod/endpoints/mod_aes67/aes67_api.h
@@ -3,7 +3,7 @@
 
 #include <gst/gst.h>
 #include <switch.h>
-#define MOD_AES_VERSION_DATE "2025-09-10"
+#define MOD_AES_VERSION_DATE "2025-12-16"
 #define DIRECTION_TX 1 << 0
 #define DIRECTION_RX 1 << 1
 
